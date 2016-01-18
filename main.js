6d58d9cb3a244132cb4bbf8f0865f9bb79d80839
Lijy91 <lijy91@foxmail.com>
2016-01-18 14:45:32 +0800

    增加db.json缓存处理

--- main.js
+++ main.js
@@ -43,56 +43,74 @@ if (token == "") {
 
     if (argv.q || argv.query) {
         query = typeof (argv.q || argv.query) === 'string' ? (argv.q || argv.query).trim() : '';
-        queryReg = new RegExp('[' + query + ']+', 'i')
+        queryReg = new RegExp(query, "i")
     }
 
     if (query.length === 0) {
         error("错误：搜索参数设置错误，请按 Return 键查看配置说明", "https://github.com/lijy91/alfred-coding-workflow", "https://github.com/lijy91/alfred-coding-workflow");
     } else {
-        request.get('https://coding.net/api/user/projects?page=1&pageSize=100&access_token=' + token, {
-        }, function (error, response, body) {
-            // 没有 response 数据，或者状态码不为 200
-            if (response === undefined ||
-                response.statusCode !== 200) {
-                // TODO: 错误消息提醒
-            } else {
-                try {
-                    // 清空旧数据
-                    db('projects').remove({});
-                    workflow.clearItems();
-                    var result = JSON.parse(body);
-                    // 返回的状态码不为 0
-                    if (result.code !== 0) {
-                        console.log(result.code)
-                        return;
-                    }
-                    result.data.list.forEach(function(project) {
-                        db('projects').push({
-                            id: project.id,
-                            name: project.name,
-                            description: project.description,
-                            https_url: project.https_url,
-                        });
-
-                        if (queryReg.test(project.name)) {
-                            var projectItem = new Item({
-                                uid: project.id,
-                                title: project.name,
-                                subtitle: project.description,
-                                valid: true, icon: AlfredNode.ICONS.WEB,
-                                arg: project.https_url,
-                                autocomplete: "autocomplete"}
-                            );
-                            workflow.addItem(projectItem);
-                        }
-                    });
-                    workflow.feedback();
-                } catch(e) {
-                    // Not a valid JSON response
-                    console.log('Not a valid JSON response');
+        if (db('projects').size() > 0) {
+            var projects = db('projects').value();
+            projects.forEach(function(project) {
+                if (queryReg.test(project.name)) {
+                    var projectItem = new Item({
+                        uid: project.id,
+                        title: project.name,
+                        subtitle: project.description,
+                        valid: true, icon: AlfredNode.ICONS.WEB,
+                        arg: project.https_url,
+                        autocomplete: "autocomplete"}
+                    );
+                    workflow.addItem(projectItem);
                 }
-            }
-        });
+            });
+            workflow.feedback();
+        } else {
+            request.get('https://coding.net/api/user/projects?page=1&pageSize=100&access_token=' + token, {
+            }, function (error, response, body) {
+                // 没有 response 数据，或者状态码不为 200
+                if (response === undefined ||
+                    response.statusCode !== 200) {
+                    // TODO: 错误消息提醒
+                } else {
+                    try {
+                        // 清空旧数据
+                        db('projects').remove({});
+                        workflow.clearItems();
+                        var result = JSON.parse(body);
+                        // 返回的状态码不为 0
+                        if (result.code !== 0) {
+                            console.log(result.code)
+                            return;
+                        }
+                        result.data.list.forEach(function(project) {
+                            db('projects').push({
+                                id: project.id,
+                                name: project.name,
+                                description: project.description,
+                                https_url: project.https_url,
+                            });
+
+                            if (queryReg.test(project.name)) {
+                                var projectItem = new Item({
+                                    uid: project.id,
+                                    title: project.name,
+                                    subtitle: project.description,
+                                    valid: true, icon: AlfredNode.ICONS.WEB,
+                                    arg: project.https_url,
+                                    autocomplete: "autocomplete"}
+                                );
+                                workflow.addItem(projectItem);
+                            }
+                        });
+                        workflow.feedback();
+                    } catch(e) {
+                        // Not a valid JSON response
+                        console.log('Not a valid JSON response');
+                    }
+                }
+            });
+        }
     }
 }
 
